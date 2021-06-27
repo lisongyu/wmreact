f2e9f96ad855fc878a77b132cb0a29d1b0426b75
lisongyu <269876940@qq.com>
2021-06-27 08:34:00 +0800

    提交初事文件提交

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
+// class ChildCpn extends Component {
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>子组件展示数据:{this.props.childInfo}</h2>
+//       </div>
+//     )
+//   }
+// }
+
+function ChildCpn(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>子组件展示数据:{props.childInfo}</h2>
     </div>
-  );
+  )
+
 }
 
-export default App;
+ChildCpn.defaultProps = {
+  childInfo: '我是默认值'
+}
+
+
+
+export default class App extends Component {
+  constructor() {
+    super();
+    console.log('执行了组件的constructor')
+    this.state = {
+      message: "你好啊，李银河",
+      childInfo: '我是父亲传来的'
+    }
+  }
+  render() {
+    console.log('执行了render')
+    let { childInfo } = this.state
+    return (
+      <div>
+        <span>我是App组件</span>
+        <h2>{this.state.message}</h2>
+        <ChildCpn />
+      </div>
+    )
+  }
+  componentDidMount() {
+    console.log('执行了组件的didmount')
+  }
+}
