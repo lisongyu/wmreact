a17522d29c01f7b824a42f2e8f62be5ce42eac10
lisongyu <269876940@qq.com>
2021-07-11 21:44:52 +0800

    connnect完成

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -5,6 +5,23 @@ import store from "../store"
 export function connect(mapStateToProps, mapDispachToProp) {
   return function enhanceHOC(WrappedComponent) {
     return class extends PureComponent {
+
+      constructor(props) {
+        super(props)
+        this.state = {
+          storeState:mapStateToProps(store.getState())
+        }
+      }
+      componentDidMount() {
+        this.unsubscribe=store.subscribe(() => {
+          this.setState({
+            storeState:mapStateToProps(store.getState())
+          })
+        })
+      }
+      componentWillUnmount() {
+        this.unsubscribe()
+      }
       render() {
         return <WrappedComponent {...this.props}
           {...mapStateToProps(store.getState())}
