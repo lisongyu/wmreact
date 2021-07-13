d2679250870a957bca279aaed08c0defbbeea738
lisongyu <269876940@qq.com>
2021-07-13 22:25:23 +0800

    提交工具

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,21 @@
-import {createStore,applyMiddleware} from 'redux'
+
 import thunkMiddleware from 'redux-thunk'
 import reducer from './reducer.js'
 
-const storeEnhancer=applyMiddleware(thunkMiddleware)
-const store = createStore(reducer,storeEnhancer)
+
+import { createStore, applyMiddleware, compose } from 'redux';
+
+const composeEnhancers =
+  typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunkMiddleware),
+  // other store enhancers if any
+);
+const store = createStore(reducer, enhancer)
 export default store
