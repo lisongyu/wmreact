1526f6394eda3489e15f013089652f67d04bc532
lisongyu <269876940@qq.com>
2021-07-25 10:52:31 +0800

    useReducer使用

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { createContext } from 'react'
+// import React, { createContext } from 'react'
+import React from 'react'
 // import CounterHook from './hooks/count'
 // import addfriend from './hooks/count'
 // import AddFriend from './hooks/addfriend';
@@ -8,12 +9,13 @@ import React, { createContext } from 'react'
 // import Subscr from './hooks/useEffect/subscr';
 
 // import MoreUse from './hooks/useEffect/moreUse';
-import ContextHook from './hooks/useContext'
+// import ContextHook from './hooks/useContext'
 
 
+import CountHome from './hooks/useReducer/home';
 
-export const UseContext = createContext()
-export const ThemeContext = createContext()
+// export const UseContext = createContext()
+// export const ThemeContext = createContext()
 export default function App() {
 
   // const [show, setShow] = useState(true)
@@ -23,11 +25,12 @@ export default function App() {
       {/* {show && <Subscr />}
       <button onClick={e => setShow(!show)}>切换</button> */}
       {/* <MoreUse /> */}
-      <UseContext.Provider value={{ name: 'why', age: 18 }}>
+      {/* <UseContext.Provider value={{ name: 'why', age: 18 }}>
         <ThemeContext.Provider value={{ color: 'red' }}>
           <ContextHook />
         </ThemeContext.Provider>
-      </UseContext.Provider>
+      </UseContext.Provider> */}
+      <CountHome />
 
     </div>
   )
---
+++ src/hooks/useReducer/reducer.js
@@ -0,0 +1,8 @@
+export function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { ...state, counter: state.counter + 1 }
+    case "decrement":
+      return { ...state, counter: state.counter - 1 }
+  }
+}
